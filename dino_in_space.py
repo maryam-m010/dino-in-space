a656bbfb67e2b475f4cbf3a3196d9253f78a1326
maryam-m010 <mmaryam7@outlook.com>
2024-12-19 17:47:38 +0000

    Update dino_in_space.py

--- dino_in_space.py
+++ dino_in_space.py
@@ -15,12 +15,32 @@ class Dino(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
 
+    def update(self, key_pressed):
+        if key_pressed[pygame.K_UP]:
+            self.rect.move_ip(0, -5)
+
+        if key_pressed[pygame.K_DOWN]:
+            self.rect.move_ip(0, 5)
+
+        if key_pressed[pygame.K_LEFT]:
+            self.rect.move_ip(-5, 0)
+
+        if key_pressed[pygame.K_RIGHT]:
+            self.rect.move_ip(5, 0)
+        
 d1 = Dino(200, 200)
+d2 = Dino(300, 300)
+d3 = Dino(100, 100)
+d4 = Dino(400, 400)
+d5 = Dino(500, 500)
 dino_grp = pygame.sprite.Group()
-dino_grp.add(d1)
+dino_grp.add(d1, d2, d3, d4, d5)
 
 while True:
+    screen.fill("white")
     dino_grp.draw(screen)
+    key_pressed = pygame.key.get_pressed()
+    d1.update(key_pressed)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
